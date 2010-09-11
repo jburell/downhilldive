d7885d43c2b677b4eed380476484b5ec86af2b9a
johan.burell@gmail.com <johan.burell@gmail.com@4669b456-17ec-8e7c-1913-c99b133e57a4>
2010-09-11 16:11:50 +0000

    game loop

--- src/org/TAONGAD/DownhillDive/RenderView.java
+++ src/org/TAONGAD/DownhillDive/RenderView.java
@@ -1,6 +1,7 @@
 package org.TAONGAD.DownhillDive;
 
 import android.content.Context;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 
@@ -30,7 +31,7 @@ import android.view.View;
 
 //-----------------------------------------------
 
-public class RenderView extends View {
+public class RenderView extends SurfaceView implements SurfaceHolder.Callback {
 
 	public static final long FPS_MS = 1000 / 2;
 
@@ -44,8 +45,7 @@ public class RenderView extends View {
 	private Paint mWinPaint;
 	private Paint mLinePaint;
 
-	Bitmap m_groundTex;
-	Bitmap m_bmpAv[];
+	
 
 	public RenderView(Context context/* , AttributeSet attrs */) {
 		super(context/* , attrs */);
@@ -60,73 +60,25 @@ public class RenderView extends View {
 		mWinPaint.setColor(0xFFFF0000);
 		mWinPaint.setStrokeWidth(10);
 		mWinPaint.setStyle(Style.STROKE);
+
+		setKeepScreenOn(true);
+		getHolder().addCallback(this);
+
 		
-		m_bmpAv = new Bitmap[Avatar.Leaning.values().length];
-		try {
-			m_bmpAv[Avatar.Leaning.NONE.ordinal()] = BitmapFactory.decodeStream(DownhillDive.getInstance()
-					.getAssets().open(DownhillDive.getInstance().getAvatar().getLeanPic(Avatar.Leaning.NONE)));
-		} catch (IOException e) {
-			Log.e("RENDERER", "LOADING " + DownhillDive.getInstance().getAvatar().getLeanPic(Avatar.Leaning.NONE) + " failed!");
-			e.printStackTrace();
-		}
-		
-		try {
-			m_bmpAv[Avatar.Leaning.ALOT.ordinal()] = BitmapFactory.decodeStream(DownhillDive.getInstance()
-					.getAssets().open(DownhillDive.getInstance().getAvatar().getLeanPic(Avatar.Leaning.ALOT)));
-		} catch (IOException e) {
-			Log.e("RENDERER", "LOADING " + DownhillDive.getInstance().getAvatar().getLeanPic(Avatar.Leaning.ALOT) + " failed!");
-			e.printStackTrace();
-		}
-		
-		try {
-			m_bmpAv[Avatar.Leaning.SLIGHTLY.ordinal()] = BitmapFactory.decodeStream(DownhillDive.getInstance()
-					.getAssets().open(DownhillDive.getInstance().getAvatar().getLeanPic(Avatar.Leaning.SLIGHTLY)));
-		} catch (IOException e) {
-			Log.e("RENDERER", "LOADING " + DownhillDive.getInstance().getAvatar().getLeanPic(Avatar.Leaning.SLIGHTLY) + " failed!");
-			e.printStackTrace();
-		}
 	}
 
 	// -----------------------------------------
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		
-		DownhillDive.getInstance().getAvatar().Update();
-		
-//		DownhillDive.getInstance().drawGround(canvas, m_groundTex,
-//				DownhillDive.getInstance().getHeight() >> 1, 20,
-//				DownhillDive.getInstance().getHeight(), 1, 0, 0, 0, 0);
-		
-		DownhillDive.getInstance().drawGround(canvas, m_groundTex,
-				DownhillDive.getInstance().getHeight() >> 1, 20,
-				DownhillDive.getInstance().getHeight(), 1, 0f);
-	
-	
-		canvas.drawBitmap(m_bmpAv[DownhillDive.getInstance().getAvatar().getLeanCurrent().ordinal()], new Rect(0,0,128,128), new Rect(10,10,128,128), new Paint());
-	}
-
 //	@Override
-//	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//		super(widthMeasureSpec, heightMeasureSpec);
-//		// Keep the view squared
-////		int w = MeasureSpec.getSize(widthMeasureSpec);
-////		int h = MeasureSpec.getSize(heightMeasureSpec);
-////		int d = w == 0 ? h : h == 0 ? w : w < h ? w : h;
-////		setMeasuredDimension(d, d);
+//	protected void onDraw(Canvas canvas) {
+//		super.onDraw(canvas);
+//
+//		
 //	}
 
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
-
-//		int sx = (w - 2 * MARGIN) / 3;
-//		int sy = (h - 2 * MARGIN) / 3;
-//
-//		int size = sx < sy ? sx : sy;
-//
-//		mDstRect.set(MARGIN, MARGIN, size - MARGIN, size - MARGIN);
 	}
 
 	@Override
@@ -177,5 +129,27 @@ public class RenderView extends View {
 
 			return false;
 		}
+	}
+
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+//		SizeUtil.setScreenSize(width, height);
+	}
+ 
+	public void surfaceCreated(SurfaceHolder holder) {
+//		if (mStarted) {
+//			mGameThread.resume(holder);
+//			return;
+//		}
+//		mStarted = true;
+//
+//		Rect frame = holder.getSurfaceFrame();
+//		SizeUtil.setScreenSize(frame.width(), frame.height());
+//
+//		mGameThread.start(holder, frame.width(), frame.height());
+	}
+
+	public void surfaceDestroyed(SurfaceHolder holder) {
+//		mGameThread.pause();
 	}
 }
