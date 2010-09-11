ab5fb02ff6728747d2620738a993364f2732dfb7
johan.burell@gmail.com <johan.burell@gmail.com@4669b456-17ec-8e7c-1913-c99b133e57a4>
2010-09-11 12:17:48 +0000

    Switched from GL to Canvas

--- src/org/TAONGAD/DownhillDive/DownhillDive.java
+++ src/org/TAONGAD/DownhillDive/DownhillDive.java
@@ -3,155 +3,192 @@ package org.TAONGAD.DownhillDive;
 import java.nio.ByteBuffer;
 
 import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.khronos.opengles.GL11;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
 import android.graphics.drawable.GradientDrawable.Orientation;
+import android.opengl.GLUtils;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class DownhillDive extends Activity{
+public class DownhillDive extends Activity {
 	static DownhillDive m_instance;
-	static public DownhillDive getInstance(){
+
+	static public DownhillDive getInstance() {
 		return m_instance;
 	}
-	
+
 	int m_width;
 	int m_height;
 	boolean m_isLoading = true;
 	boolean m_isTextureLoaded = false;
 	Texture m_ground;
 	
-	int getWidth(){
+	ByteBuffer m_backBuffer;
+	Bitmap m_backBitmap;
+	int[] m_texIdx;
+
+	int getWidth() {
 		return m_width;
 	}
-	
-	int getHeight(){
+
+	int getHeight() {
 		return m_height;
 	}
-	
-	boolean isLoading(){
+
+	boolean isLoading() {
 		return m_isLoading;
 	}
-	
-	boolean isLandscape(){
+
+	boolean isLandscape() {
 		return true;
 	}
-	
-	Texture getGround(){
+
+	Texture getGround() {
 		return m_ground;
 	}
-	
-	String getLoadingImage(){
+
+	String getLoadingImage() {
 		return "loading.png";
 	}
-	
+
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState){
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		m_instance = this;
 		m_width = 100;
 		m_height = 50;
 		m_isLoading = true;
-		
+
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
-			WindowManager.LayoutParams.FLAG_FULLSCREEN); 
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-		
-		init();
-		
+
+		//init();
+
+		//RenderView renderView = new RenderView(getApplicationContext());
 		RenderView renderView = new RenderView(getApplicationContext());
 		GameRenderer renderer = new GameRenderer();
-		renderView.setRenderer(renderer);
 		setContentView(renderView);
 	}
-	
-	public void init(){
+
+	public void init() {
 		TextureManager.prepare();
-		TextureAtlas atlas = new TextureAtlas(1024, 1024);
-		
+//		TextureAtlas atlas = new TextureAtlas(1024, 1024);
+
 		int w = 320;
 		int h = 200;
-		ByteBuffer byteBuffer = ByteBuffer.allocateDirect(w * h);
-		m_ground = new Texture(byteBuffer, w, h, false);
-		atlas.insert(m_ground);
+		m_backBuffer = ByteBuffer.allocateDirect(w * h);
 		
-		TextureManager.load(atlas);
+//		m_ground = new Texture(m_backBuffer, w, h, false);
+//		atlas.insert(m_ground);
+
+//		TextureManager.load(atlas);
 	}
-	
-	public void doLoading(GL10 gl){
+
+	public void doLoading(GL10 gl) {
 		m_isLoading = true;
-		
+
 		TextureManager.loadTextures(gl);
-		
+
 		// Wait for load to complete
-		while(m_isTextureLoaded == false){
-			try{
+		while (m_isTextureLoaded == false) {
+			try {
 				Thread.sleep(10);
-			}catch(InterruptedException e){
+			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 		m_isLoading = false;
 	}
-	
-	public void textureLoadComplete(){
+
+	public void textureLoadComplete() {
 		m_isTextureLoaded = true;
 	}
-	
+
 	void drawFrame(GL10 gl){
+		Canvas canvas = new Canvas(gl);
+		Paint mLinePaint = new Paint();
+        mLinePaint.setColor(0xFFFFFFFF);
+        mLinePaint.setStrokeWidth(5);
+        mLinePaint.setStyle(Style.STROKE);
+		canvas.drawLine(0, 0, 20, 10, mLinePaint);
+		
 		// Insert magic here
-		gl.glClearColor(1f, 0, 0, 1f);
-		gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT);
+//		gl.glClearColor(1f, 0, 0, 1f);
+//		gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT);
+		
+//		m_backBitmap.setPixel(20, 10, 0x00ff00);
+//		GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, m_backBitmap, 0);
+//		gl.glBindTexture(GL10.GL_TEXTURE_2D, m_texIdx[0]);
+//		GL11 g = (GL11)gl;
+		
+		
+//		Bitmap bitmap = BitmapFactory.decodeByteArray(data, offset, length)
+//		GLUtils.texImage2D()
+//		drawGround(gl, t, tex, y1, z1, y2, z2, x, xmul, ymul, texoff)
 	}
-	
-	void drawGround(GL10 gl, Texture t, Texture tex, int y1, float z1, int y2, float z2, float x, float xmul, float ymul, float texoff){
 
-			float curvdiv = z1/(3.141592654f*0.5f);
-			float zpos = 1.0f /z2;
-			z1 = 1.0f / z1;
+	void drawGround(Canvas canvas, Bitmap groundTex, int y1,
+			float z1, int y2, float z2, float x, float xmul, float ymul,
+			float texoff) {
 
+		float curvdiv = z1 / (3.141592654f * 0.5f);
+		float zpos = 1.0f / z2;
+		z1 = 1.0f / z1;
 
-			/* sample 16 times for each scanline */
-			y1 <<= 4;
-			y2 <<= 4;
-			ymul *= 16.0f;
+		/* sample 16 times for each scanline */
+		y1 <<= 4;
+		y2 <<= 4;
+		ymul *= 16.0f;
 
-			int BottomY = y2;
-			float zadd = (z1 - zpos) / (float)(y2 - y1);
-			while(y2 > y1)
+		int BottomY = y2;
+		float zadd = (z1 - zpos) / (float) (y2 - y1);
+		while (y2 > y1) {
+			float z = 1.0f / zpos;
+
+			/*
+			 * NOTE: z is the actual depth of this scanline, zpos is 1/z. The
+			 * most common perspective projection formula is used, i.e.
+			 * 
+			 * transformed_x = (centre of screen) - (half width of screen)*(x /
+			 * z)
+			 * 
+			 * But instead of dividing by z we can multiply by zpos if we like.
+			 */
+			float centx = (float) (x + xmul * (Math.cos(z / curvdiv) - 1.0f));
+			int topy = (int) (y2 + (m_height >> 1)
+					* (ymul * (Math.cos(z / curvdiv) - 1.0f) / z));
+
+			if ((topy >> 4) < (BottomY >> 4)) /*
+											 * question is: are we at least one
+											 * pixel line above what was last
+											 * drawn ?
+											 */
 			{
-				float z = 1.0f / zpos;
-
-				/*
-					NOTE: z is the actual depth of this scanline, zpos is 1/z. The most common
-					perspective projection formula is used, i.e.
-
-					transformed_x = (centre of screen) - (half width of screen)*(x / z)
-
-					But instead of dividing by z we can multiply by zpos if we like.
-				*/
-				float centx = (float) (x + xmul*(Math.cos(z/curvdiv) - 1.0f));
-				int topy = (int) (y2 + (m_height >> 1)*(ymul*(Math.cos(z/curvdiv) - 1.0f) / z));
-
-				if((topy >> 4) < (BottomY >> 4)) /* question is: are we at least one pixel line above what was last drawn ? */
-				{
-//					stretch_blit(tex, t, 
-//							0, (int)((z+texoff)*4.0f)&(tex.getHeight() - 1),
-//							tex.getWidth(), 1,
-//							(m_width >> 1) + (m_width >> 1)*(centx-1.0f)*zpos, (topy) >> 4,
-//							(m_width >> 1) * (2.0f)*zpos, (BottomY >> 4) - (topy >> 4));
-					BottomY = topy;
-				}
-
-				zpos += zadd;
-		 		y2--;
+				// stretch_blit(groundTex, backBuffer,
+				// 0, (int)((z+texoff)*4.0f) & (groundTex.getHeight() - 1),
+				// groundTex.getWidth(), 1,
+				// (m_width >> 1) + (m_width >> 1)*(centx-1.0f)*zpos, (topy) >>
+				// 4,
+				// (m_width >> 1) * (2.0f)*zpos, (BottomY >> 4) - (topy >> 4));
+				BottomY = topy;
 			}
+
+			zpos += zadd;
+			y2--;
 		}
+	}
 }
